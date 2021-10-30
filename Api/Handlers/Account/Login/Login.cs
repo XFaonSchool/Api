78bb7fe0e645d135e167e924af982480c6d72195
XfaonAE <xfaonae@gmail.com>
2021-10-30 09:29:48 -0400

    Add project files.

---
+++ Api/Handlers/Account/Login/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Handlers.Account.Login
+{
+    internal class Login
+    {
+    }
+}
---
+++ Api/Server.cs
@@ -0,0 +1,33 @@
+﻿using Exolix.ApiHost;
+using Exolix.Terminal;
+
+public class GlobalStorage
+{
+    public static ApiHost? Api;
+}
+
+class Server
+{
+    public static void Main(string[] args)
+    {
+        Logger.Info("Axeri API GateWay Server");
+        Logger.Info("Starting API gateway server");
+
+        ApiHost api = GlobalStorage.Api = new ApiHost(new ApiHostSettings
+        {
+            Port = 9080
+        });
+
+        api.OnReady(() =>
+        {
+            Logger.Success("Server is ready");
+
+            api.OnOpen((connection) =>
+            {
+                Logger.Info($"New connection opened from '{connection.RemoteAddress}'");
+            });
+        });
+
+        api.Run();
+    }
+}
