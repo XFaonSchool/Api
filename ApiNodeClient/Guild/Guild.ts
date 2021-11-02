3b40618b219ebbf1e4cbe9e9e171d51da47eac7c
XfaonAE <xfaonae@gmail.com>
2021-11-02 11:38:44 -0400

    Added non called events for guild

--- ApiNodeClient/Guild/Guild.ts
+++ ApiNodeClient/Guild/Guild.ts
@@ -1,15 +1,34 @@
 import { ExolixApi } from "@axeridev/exolix-node";
 
+export interface GuildJoinReturnMessage {
+	Identifier: string;
+}
+
 export class Guild {
 	public api: ExolixApi;
 
+	private onJoinSuccessEvents: ((identifier: string) => void)[] = [];
+	private onJoinFailedEvents: ((identifier: string, reason: "member-already-exists" | "banned") => void)[] = [];
+
 	public constructor(api: ExolixApi) {
 		this.api = api;
+
+		api.onMessage<GuildJoinReturnMessage>("guild:join _reply:success", (message) => {
+
+		});
 	}
 
 	public joinGuild(identifier: string) {
 		this.api.send("guild:join", {
 			Identifier: identifier
 		});
+	}
+
+	public onJoinSuccess(action: (identifier: string) => void) {
+		this.onJoinSuccessEvents.push(action);
+	}
+
+	public onJoinFailed(action: (identifier: string, reason: "member-already-exists" | "banned") => void) {
+		this.onJoinFailedEvents.push(action);
 	}
 }
