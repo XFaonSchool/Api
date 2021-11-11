3f1174641eeb37cf9a1a85104b37f322c1ed4c34
XfaonAE <xfaonae@gmail.com>
2021-11-11 07:02:13 -0500

    Fixed infinite loading issue

--- webclient/routes/MainPage.tsx
+++ webclient/routes/MainPage.tsx
@@ -1,50 +1,8 @@
 import * as React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { api } from "../pages/_app";
-import { DefaultButton, Dialog, DialogFooter, PrimaryButton, ProgressIndicator, Spinner } from "@fluentui/react";
 import { AppLayout } from "./_shared/AppLayout/AppLayout";
-import { GuildProfile } from "../../apiNodeClient/Guild/Guild";
-
-let onLoginStateUpdated = (newState: string) => { };
-let onLoginError = (message: string, error: string) => { };
-
-let loggedIn = false;
-
-api.account.onLoginTokenSuccess(() => {
-	loggedIn = true;
-	onLoginStateUpdated("loggedIn");
-});
-
-api.account.onLoginTokenFailed((reason) => {
-	loggedIn = false;
-
-	if (reason == "invalid") {
-		onLoginError("Failed to login because you have an invalid token. This may sometimes happen when you account password is updated", reason);
-		return;
-	}
-
-	onLoginError("Failed to login via token", reason);
-});
-
-export function tryLogin() {
-	if (api.ready && typeof window != undefined && !loggedIn) {
-		api.account.loginToken(localStorage.getItem("token"));
-	}
-} 
-
-api.onReady(() => {
-	console.info("API is ready");
-	if (typeof window != "undefined") {
-		tryLogin();
-	}
-});
-
-let guildsFetched = (guilds: GuildProfile[]) => { };
-
-api.guild.onGetAllCurrent((guilds) => {
-	guildsFetched(guilds);
-});
+import { LoginManager } from "./_shared/LoginManager/LoginManager";
 
 export function MainPage() {
 	const [loggedIn, setLoggedInState] = useState<"checking" | "loggedIn" | "loggedOut">("checking");
@@ -54,50 +12,11 @@ export function MainPage() {
 	const navigate = useNavigate();
 	const [guilds, setGuilds] = useState<any>([]);
 
-	guildsFetched = (g) => {
-		setGuilds(g);
-	}
-
-	onLoginError = (error, reason) => {
-		console.error("Err: " + reason);
-
-		setLoginError(error);
-		setErrorReason(reason);
-	}
-
-	if (!localStorage.getItem("token")) {
-		navigate("/login?redirect-reason=not-logged-in");
-		return (<></>);
-	}
-
-	onLoginStateUpdated = (newState: any) => {
-		setLoggedInState(newState);
-
-		if (newState == "loggedIn") {
-			api.guild.getAllCurrent();
-		}
-	}
-
 	return (
 		<div className="_page_main-page">
 			<AppLayout>
-				<Dialog hidden={!loginError} title="Failed to login">
-					{loginError}
-					<strong>Reason: {}</strong>
-
-					<DialogFooter>
-						<PrimaryButton onClick={() => { localStorage.removeItem("token"); navigate("/login"); }}>Go to Login</PrimaryButton>
-						<DefaultButton onClick={() => { setLoginError(false); tryLogin(); }}>Try Again</DefaultButton>
-					</DialogFooter>
-				</Dialog>
-
-				<h1>Guilds</h1>
-
-				{guilds?.map((g) => {
-					return <p>Guild: {g.DisplayName}</p>
-				})}
-
-				<PrimaryButton onClick={() => tryLogin()}>Try Login</PrimaryButton>
+				<LoginManager />
+				<h1>Home</h1>
 			</AppLayout>
 		</div>
 	);
---
+++ webclient/routes/_shared/LoginManager/LoginManager.tsx
@@ -0,0 +1,51 @@
+import { Dialog } from "@fluentui/react";
+import { useState } from "react";
+import { api } from "../../../pages/_app";
+
+let isLoggedIn = false;
+let apiReadyOnInit = api.ready;
+
+const events = {
+    onLoginSuccess: () => { },
+    onLoginFailed: () => { },
+};
+
+api.account.onLoginTokenSuccess(() => {
+    isLoggedIn = true;
+    events.onLoginSuccess();
+});
+
+function runLoggin() {
+    if (!isLoggedIn && api.ready) {
+        api.account.loginToken("tok.en");
+    }
+}
+
+if (!apiReadyOnInit) {
+    api.onReady(() => runLoggin());
+}
+
+export function LoginManager() {
+    const [status, setStatus] = useState("working");
+
+    events.onLoginSuccess = () => {
+        setStatus("success");
+    }
+
+    runLoggin();
+
+    return (
+        <div>
+            {status == "working"
+                ? <h1>Logging In...</h1>
+                : (status == "success"
+                    ? <h1>Logged In</h1>
+                    : <h1>Failed to login</h1>)
+            }
+
+            <Dialog hidden={true} title="Failed you log you in">
+                An unexpected error occoured. We couldn't log you in!
+            </Dialog>
+        </div>
+    );
+}
